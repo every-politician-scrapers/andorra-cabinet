507497bc0babdd462e6b844df9c9c2a0259dbe83
Tony Bowden <tony@tmtm.com>
2021-08-07 13:23:30 +0300

    Update to every-politician-scraper 0.1.0

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -5,25 +5,17 @@ require 'every_politician_scraper/scraper_data'
 require 'pry'
 
 class MemberList
-  # details for an individual member
-  class Member < Scraped::HTML
-    field :name do
+  class Member
+    def name
       noko.css('.ministre').xpath('text()').map(&:text).join(' ').tidy
     end
 
-    field :position do
+    def position
       noko.css('.ministeri').xpath('text()').map(&:text).join(' ').tidy
     end
   end
 
-  # The page listing all the members
-  class Members < Scraped::HTML
-    field :members do
-      member_container.map { |member| fragment(member => Member).to_h }
-    end
-
-    private
-
+  class Members
     def member_container
       noko.css('#system table')
     end
